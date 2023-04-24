e9b6a349b0b4f55c8132c2341e6cc26ed326317b
Gleefre <varedif.a.s@gmail.com>
2023-04-24 18:54:51 +0200

    Add note about api levels

--- api-levels.lisp
+++ api-levels.lisp
@@ -1,5 +1,7 @@
 (in-package #:cl-android/cffi)
 
+;; Note: these functions are part of android libc, not libandroid!
+
 (defcfun (get-application-target-api-level "android_get_application_target_sdk_version") :int
   "Returns the target API level of the caller
 Returns __ANDROID_API_FUTURE__ if there is no known target API level.")
