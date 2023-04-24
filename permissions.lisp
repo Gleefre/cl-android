8bff8ba1ad4c997d9b9397d82829efa651629e09
Gleefre <varedif.a.s@gmail.com>
2023-04-24 20:48:09 +0200

    Scrapped all known permissions from docs

--- permissions.lisp
+++ permissions.lisp
@@ -19,10 +19,25 @@ Java API of Context::checkPermission() is usually faster and is preferred."
 
 (in-package #:cl-android)
 
+(defparameter *known-permissions* (make-hash-table :test 'eql))
+
+(defun lispy (value)
+  (intern (substitute #\- #\_ (subseq value (1+ (position #\. value :from-end t))))
+          '#:keyword))
+
+(defun register-permission (value &optional (name (lispy value)))
+  (setf (gethash name *known-permissions*) value))
+
+(defun register-permissions (file)
+  (mapcar #'register-permission
+          (uiop:read-file-lines file)))
+
+(register-permissions (asdf:system-relative-pathname :cl-android "permissions.txt"))
+
 (defun check-permission (permission &key (pid (android:getpid))
                                          (uid (android:getuid)))
   (with-foreign-object (result 'android:check-permission-result)
-    (let* ((permission (substitute #\_ #\- (format nil "android.permission.~a" permission)))
+    (let* ((permission (gethash permission *known-permissions* (princ-to-string permission)))
            (status (android:check-permission permission pid uid result))
            (result (mem-aref result 'android:check-permission-result)))
       (if (eq status :ok)
