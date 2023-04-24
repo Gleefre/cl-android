170fea4865d5225335cf540285679db9599336e4
Gleefre <varedif.a.s@gmail.com>
2023-04-24 18:55:06 +0200

    Fix libandroid

--- packages.lisp
+++ packages.lisp
@@ -1,12 +1,13 @@
 (defpackage #:cl-android/cffi
   (:use #:cl #:cffi)
-  (:export #:get-application-target-api-level
+  (:export #:libandroid
+           #:get-application-target-api-level
            #:get-device-api-level))
 
 (defpackage #:cl-android
   (:use #:cl #:cffi #:cl-android/cffi)
-  (:export #:init
+  (:export #:libandroid
+           #:init
            #:quit
-
            #:get-application-target-api-level
            #:get-device-api-level))
