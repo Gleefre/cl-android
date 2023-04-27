4ee7beb44c049c37fab9636101f558ead320076a
Gleefre <varedif.a.s@gmail.com>
2023-04-27 12:00:21 +0200

    Clean up native-helper, add something more useful

--- native-helper.lisp
+++ native-helper.lisp
@@ -21,114 +21,16 @@
                       collect (mem-aref return-vms 'jll:vm i)))
               status))))
 
-#|
-(defun destroy-vm (vm)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref jvm 'jni:java-vm)
-                                               '(:struct jni:jni-invoke-interface)
-                                               'jni:destroy-java-vm)
-                           ()
-                           jni:java-vm jvm
-                           jni:return-code))
+(defun jstring-to-string (env jstring)
+  (let* ((length (jll:get-string-utf-length env jstring))
+         (chars (jll:get-string-utf-chars env jstring)))
+    (unwind-protect (coerce (loop for i below length
+                                  collect (code-char (cffi:mem-aref chars :char i)))
+                            'string)
+      (jll:release-string-utf-chars env jstring chars))))
 
-(defun attach-current-thread (jvm &key (as-daemon nil)
-                                       (version :version-1-6 version-p)
-                                       (thread-name (null-pointer) thread-name-p)
-                                       (thread-group (null-pointer) thread-group-p))
-  ;; To bring wrap-if in!
-  (with-foreign-objects ((ret-environment 'jni:jni-environment))
-    (let ((status (foreign-funcall-pointer (foreign-slot-value (mem-aref jvm 'jni:java-vm)
-                                                               '(:struct jni:jni-invoke-interface)
-                                                               (if as-daemon
-                                                                   'jni:attach-current-thread
-                                                                   'jni:attach-current-thread-as-daemon))
-                                           ()
-                                           jni:java-vm jvm
-                                           jni:jni-environment ret-environment
-                                           (:pointer (:struct jni:java-vm-attach-args)) (null-pointer)
-                                           jni:return-code)))
-      (values (when (eq status :ok)
-                (mem-aref ret-environment 'jni:jni-environment))
-              status))))
-
-(defun detach-current-thread (jvm)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref jvm 'jni:java-vm)
-                                               '(:struct jni:jni-invoke-interface)
-                                               'jni:detach-current-thread)
-                           ()
-                           jni:java-vm jvm
-                           jni:return-code))
-
-(defun get-environment (jvm &optional (version :version-1-6))
-  (with-foreign-objects ((ret-environment 'jni:jni-environment))
-    (let ((status (foreign-funcall-pointer (foreign-slot-value (mem-aref jvm 'jni:java-vm)
-                                                               '(:struct jni:jni-invoke-interface)
-                                                               'jni:get-environment)
-                                           ()
-                                           jni:java-vm jvm
-                                           jni:jni-environment ret-environment
-                                           jni:jni-version version
-                                           jni:return-code)))
-      (values (when (eq status :ok)
-                (mem-aref ret-environment 'jni:jni-environment))
-              status))))
-
-(defun new-string-utf (java-environment string)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref java-environment 'jni:jni-environment)
-                                               '(:struct jni:jni-native-interface)
-                                               'jni:new-string-utf)
-                           ()
-                           jni:jni-environment java-environment
-                           :string string
-                           jni:java-string))
-
-(defun decode-string-utf (java-environment java-string)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref java-environment 'jni:jni-environment)
-                                               '(:struct jni:jni-native-interface)
-                                               'jni:get-string-utf-chars)
-                           ()
-                           jni:jni-environment java-environment
-                           jni:java-string java-string
-                           :pointer (null-pointer)
-                           :string))
-
-(defun java-find-class (java-environment class-name)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref java-environment 'jni:jni-environment)
-                                               '(:struct jni:jni-native-interface)
-                                               'jni:find-class)
-                           ()
-                           jni:jni-environment java-environment
-                           :string class-name
-                           jni:java-class))
-
-(defun get-static-field-id (environment class name signature)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref environment 'jni:jni-environment)
-                                               '(:struct jni:jni-native-interface)
-                                               'jni:get-static-field-id)
-                           ()
-                           jni:jni-environment environment
-                           jni:java-class class
-                           :string name
-                           :string signature
-                           jni:java-field-id))
-
-(defun get-static-object-field (environment class field)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref environment 'jni:jni-environment)
-                                               '(:struct jni:jni-native-interface)
-                                               'jni:get-static-object-field)
-                           ()
-                           jni:jni-environment environment
-                           jni:java-class class
-                           jni:java-field-id field
-                           jni:java-object))
-
-(defun get-method-id (environment class name signature)
-  (foreign-funcall-pointer (foreign-slot-value (mem-aref environment 'jni:jni-environment)
-                                               '(:struct jni:jni-native-interface)
-                                               'jni:get-method-id)
-                           ()
-                           jni:jni-environment environment
-                           jni:java-class class
-                           :string name
-                           :string signature
-                           jni:java-method-id))
-|#
+(defun permission-name (env name)
+  (let* ((class (jll:find-class env "android/Manifest$permission"))
+         (field-id (jll:get-static-field-id env class name "Ljava/lang/String;"))
+         (perm (jll:get-static-object-field env class field-id)))
+    (jstring-to-string env perm)))
